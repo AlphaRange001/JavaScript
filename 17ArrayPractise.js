9832ef50773b0d4a5c65f179a79c0193873bcab6
AlphaRange001 <vp181431@gmail.com>
2024-10-17 01:13:42 +0530

    Day-6

--- 17ArrayPractise.js
+++ 17ArrayPractise.js
@@ -1,26 +1,42 @@
-let arr = [1, 23, 3, 7, 3, 4, 5]
-let max = arr[0]
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-        max = arr[i]
-    }
-}
-console.log(max)
+let arr = [1, 23, 3, 57, 3, 4, 5]
+// let max = arr[0]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
 
 
-console.log(arr.reduce((x,y)=>{
+// console.log(arr.reduce((x,y)=>{
+//     return x+y
+// }))
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i]
+// }
+// console.log(sum)
+// let arr1 = []
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (!arr1.includes(arr[i])) {
+//         arr1.push(arr[i])
+//     }
+
+// } console.log(arr1)
+let sum=arr.reduce((x,y)=>{
     return x+y
-}))
-let sum = 0
-for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i]
-}
-console.log(sum)
-let arr1 = []
-for (let i = 0; i < arr.length; i++) {
-
-    if (!arr1.includes(arr[i])) {
-        arr1.push(arr[i])
+})
+// console.log(sum)
+let previousmax, max=arr[0]
+for(let i=0;i<arr.length;i++){
+    console.log(max)
+     previousmax=max
+    if(arr[i]>max){
+        max=arr[i]
+      
     }
-
-} console.log(arr1)
+    if{i< arr.length -1}
+}
+// console.log(previousmax)
+// console.log(max)
---
+++ 27AlertPromptConfirm.js
@@ -0,0 +1,14 @@
+
+// document.write("order canelled")
+// let go = confirm("Let's go")
+// if (go == true) {
+//     document.write(go)
+// }
+// else {
+//     document.write("you need to wait")
+// }
+// let c=alert("Danger Zone")
+// let d=prompt("Enter value","66")
+// document.write(d)
+console.log(window)
+console.log(document)
