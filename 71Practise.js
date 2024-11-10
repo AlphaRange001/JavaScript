6b008c99e00b935b4c7520a6167d94f4478a955c
AlphaRange001 <vp181431@gmail.com>
2024-11-11 03:18:25 +0530

    Day -23

--- 71Practise.js
+++ 71Practise.js
@@ -1,6 +1,23 @@
-let a=fetch("https://api.spacexdata.com/v5/launches/latest")
-a.then((a)=>{
-    return a.json()
-}).then((ab)=>{
-    console.log(ab)
-})
+
+async function again(){
+let a = await fetch("https://api.adviceslip.com/advice")
+let ab=await a.json()
+console.log(ab)
+
+    ihtml = ""
+    ihtml += `  
+        <div class="card" style="width: 18rem;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR132TBAD0-GhGhN8_2Xr-3obkFd4NzFbk6Hg&s" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Today's Thought</h5>
+            <p class="card-text">${ab.slip.advice}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>`
+
+    cardcontainer.innerHTML = ihtml
+
+}
+again()
+
+
