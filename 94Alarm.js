31831b6868900d81417105d446d329b70489de13
AlphaRange001 <vp181431@gmail.com>
2024-11-23 03:10:44 +0530

    Day -35

---
+++ 94Alarm.js
@@ -0,0 +1,9 @@
+// let num = prompt("enter time")
+// num = parseInt(num);
+// function alarm() {
+//     setTimeout(() => {
+//         let audio = new Audio('alert_tone.mp3')
+//         audio.play()
+//     }, num* 1000)
+// }
+// alarm()
