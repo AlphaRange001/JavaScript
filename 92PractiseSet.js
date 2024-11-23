e51b3a0f552997544e7da9420e74a7b9bcb15512
AlphaRange001 <vp181431@gmail.com>
2024-11-24 01:03:41 +0530

    Day-36

--- 92PractiseSet.js
+++ 92PractiseSet.js
@@ -10,34 +10,34 @@ two()
 
 // 2nd 
 
-function arr(a,b,c){
-return (a+b+c)/3
-}
-let z=[1,2,3]
-console.log(arr(...z))
+// function arr(a,b,c){
+// return (a+b+c)/3
+// }
+// let z=[1,2,3]
+// console.log(arr(...z))
 
 
 // 3rd 
-let a = prompt("enter no.")
-a = parseInt(a);
-    (function js() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                console.log(`I am ${a} sec late`);
-                resolve(a)
-            }, a * 1000);
-        })
-    })()
+// let a = prompt("enter no.")
+// a = parseInt(a);
+//     (function js() {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 console.log(`I am ${a} sec late`);
+//                 resolve(a)
+//             }, a * 1000);
+//         })
+//     })()
 
 // 4th
 
-function intrest(a,b,c){
-    return (a*b*c)/100
-}
-let a=prompt("enter amount")
-a=parseInt(a);
-let r=prompt("enter rate")
-r=parseInt(r);
-let t=prompt("enter time")
-t=parseInt(t);
-intrest(a,r,t)
+// function intrest(a,b,c){
+//     return (a*b*c)/100
+// }
+// let a=prompt("enter amount")
+// a=parseInt(a);
+// let r=prompt("enter rate")
+// r=parseInt(r);
+// let t=prompt("enter time")
+// t=parseInt(t);
+// intrest(a,r,t)
--- 94Alarm.js
+++ 94Alarm.js
@@ -1,9 +1,9 @@
-// let num = prompt("enter time")
+// let num = prompt("enr time");
 // num = parseInt(num);
-// function alarm() {
-//     setTimeout(() => {
-//         let audio = new Audio('alert_tone.mp3')
-//         audio.play()
-//     }, num* 1000)
-// }
-// alarm()
+function alarm() {
+    setTimeout(() => {
+        let audio = new Audio('alert_tone.mp3')
+        audio.play();
+    }, 1000)
+}
+alarm()
